75d157159320bb0f97d0ba2ede5e34e123e1c067
carinampm99 <carinampm99@gmail.com>
2021-04-22 10:38:48 +0200

    Update Proyecto.java

--- XtremeProgrammingERP/src/modelo/Proyecto.java
+++ XtremeProgrammingERP/src/modelo/Proyecto.java
@@ -1,4 +1,26 @@
 package modelo;
+
+import java.util.ArrayList;
+
 /*EQUIPO B*/
 public class Proyecto {
+
+    private String ID;
+    private ArrayList<Story> listaStories;
+    private ArrayList<Ciclo> listaCiclos;
+    private ArrayList<MiembroEquipo> listaMiembros;
+    //metadatos
+
+    public void comenzar() {
+
+    }
+
+    public boolean aniadeStory(Story s) {
+        return listaStories.add(s);
+    }
+
+    public void cambiaStory() {
+
+    }
+
 }
